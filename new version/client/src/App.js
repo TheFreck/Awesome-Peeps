69863cb913fcb46b04c5186dc2b500dc58833d47
Kathy Erdmann <kathryn.erdmann@hsc.utah.edu>
2019-01-05 21:45:32 -0700

    Create Model

--- new version/client/src/App.js
+++ new version/client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
 import Start from "./pages/Start";
-import Create from "./pages/Start";
+import Create from "./pages/Create";
 
 const App = () => (
   <Router>
