049c6ee10ea8b50eba225f8f4997cd41e6ae2128
mcarpent87 <carpemc@gmail.com>
2019-01-10 20:11:36 -0700

    sooo it turns out we do need grid and jumbotron

--- new version/client/src/components/UserList/UserList.js
+++ new version/client/src/components/UserList/UserList.js
@@ -3,17 +3,13 @@ import React from "react";
 const UserList = props => (
   <div className="container text-center">
     <div class="col s12">
-      <h1>I'M A GREEDY BASTARD - HERE IS MY LIST</h1>
+      {/* <h1>I'M A GREEDY BASTARD - HERE IS MY LIST</h1> */}
       <button onClick={props.selectUser} value={props.uuid} data-id={props._id}>
         {props.name}{" "}
       </button>
     </div>
   </div>
 );
-<<<<<<< HEAD
-  
 
-=======
->>>>>>> 027f03cd0914fd7548dd126a4490cfbb479237b0
 
 export default UserList;
