089915513e0f094e9ac8b478282f045acfd91988
Aroha Dobson <aroha.dobson@arohamarieprogramming.com>
2019-01-10 21:07:25 -0700

    approve merges

--- new version/client/src/pages/Friends/Friends.js
+++ new version/client/src/pages/Friends/Friends.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
-import UserList from "../../components/UserList"
+import UserList from "../../components/UserList";
 import Jumbotron from "../../components/Jumbotron";
 import API from "../../utils/API";
 import { Col, Row, Container } from "../../components/Grid";
 import Nav from "../../components/Nav";
 import MainLogo from "../../components/MainLogo";
-
-
 
 class Friends extends Component {
   state = {
@@ -23,72 +21,70 @@ class Friends extends Component {
       myItems: [],
       notes: ""
     }
-  }
+  };
 
   // componentDidMount() {
   //   this.getUsers();
   // }
 
-//Find saved users
-getAllUsers = () => {
-  API.getUsers()
-    .then(res =>
-      this.setState({ users: res.data }) 
-    )
-    .catch(err => console.log(err));
+  //Find saved users
+  getAllUsers = () => {
+    API.getUsers()
+      .then(res => this.setState({ users: res.data }))
+      .catch(err => console.log(err));
   };
 
-selectUser = (event) => {
-  // console.log("This is data-id", event.target.getAttribute("data-id"))
-  console.log("This is uuid value", event.target.value)
-  // console.log("This is uuid name", event.target.name)
-  // console.log(this.state.user.login.uuid)
-  API.updateUser({
+  selectUser = event => {
+    // console.log("This is data-id", event.target.getAttribute("data-id"))
+    console.log("This is uuid value", event.target.value);
+    // console.log("This is uuid name", event.target.name)
+    // console.log(this.state.user.login.uuid)
+    API.updateUser({
       "login.uuid": event.target.value
     })
-    .then(res =>
-      {console.log("Create: ", res)
-        this.setState({ shareWithOthers: res.data }) }
-    )
-    .catch(err => console.log(err));
+      .then(res => {
+        console.log("Create: ", res);
+        this.setState({ shareWithOthers: res.data });
+      })
+      .catch(err => console.log(err));
   };
 
-renderUsers = () => {
-  return this.state.users.map(save => (
-    <UserList
-      _id={save._id}
-      key={save._id}
-      name={save.name}
-      uuid={save.uuid}
-      pic={save.pic}
-      selectUser={this.selectUser}
-    />
-  ))
-  }
+  renderUsers = () => {
+    return this.state.users.map(save => (
+      <UserList
+        _id={save._id}
+        key={save._id}
+        name={save.name}
+        uuid={save.uuid}
+        pic={save.pic}
+        selectUser={this.selectUser}
+      />
+    ));
+  };
 
-render() {
-  return (
-  
-    <div className="container bg-white">
-      <div class="Row">
-        <div class="col s12 center-align top:60px">
-          <MainLogo />
+  render() {
+    return (
+      <div className="container bg-white">
+        <div class="Row">
+          <div class="col s12 center-align top:60px">
+            <MainLogo />
+          </div>
+        </div>
+        <br />
+        <div class="Row">
+          <div class="col s12">
+            <h2 class="center-align">
+              WHICH GREEDY BASTARD DO YOU WANT TO BUY FOR?
+            </h2>
+          </div>
+        </div>
+        <div class="Row">
+          <div class="col s12 center-align">
+            <h3>UsEr bUtToNs gO heRe</h3>
+            {this.renderUsers()}
+          </div>
         </div>
       </div>
-      <br></br>
-      <div class="Row">
-        <div class="col s12">
-          <h2 class="center-align">WHICH GREEDY BASTARD DO YOU WANT TO BUY FOR?</h2>
-        </div> 
-      </div>
-      <div class="Row">
-        <div class="col s12 center-align">
-          <h3>UsEr bUtToNs gO heRe</h3>
-          {this.renderUsers()}
-        </div>
-      </div>
-
-    </div>
     );
   }
 }
---
+++ new version/client/src/pages/Landing/Landing.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class Landing extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default Landing;
