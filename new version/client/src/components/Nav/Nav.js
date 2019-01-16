191ae8cbab460e94a8281bbfd704decfa7560ab3
Aroha Dobson <aroha.dobson@arohamarieprogramming.com>
2019-01-15 19:13:07 -0700

    deleted old version

--- new version/client/src/components/Nav/Nav.js
+++ new version/client/src/components/Nav/Nav.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import SignedInLinks from './SignedInLinks';
-import SignedOutLinks from './SignedOutLinks';
+import React from "react";
+import { Link } from "react-router-dom";
+import SignedInLinks from "./SignedInLinks";
+import SignedOutLinks from "./SignedOutLinks";
 
 const Nav = () => {
-	return (
-		<nav className="nav-wrapper blue darken-3">
-			<div className="container">
-				{/* className is a Materialize classs*/}
-				<Link to="/landing" className="brand-logo">
-					Greedy Bastards List
-				</Link>{' '}
-				<SignedInLinks />
-				<SignedOutLinks />
-			</div>
-		</nav>
-	);
+  return (
+    <nav
+      className="nav-wrapper teal darken-1
+		"
+    >
+      <div className="container">
+        {/* className is a Materialize classs*/}
+        <Link to="/landing" className="brand-logo">
+          Greedy Bastards List
+        </Link>{" "}
+        <SignedInLinks />
+        <SignedOutLinks />
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
