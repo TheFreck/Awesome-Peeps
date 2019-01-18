eb36869177a255b5a7823370485696aa62fd528c
Aroha Dobson <aroha.dobson@arohamarieprogramming.com>
2019-01-17 19:57:50 -0700

    updated buttons on landing page

--- client/src/pages/Friends/Friends.js
+++ client/src/pages/Friends/Friends.js
@@ -1,56 +1,56 @@
 //----------Import external components----------
 import React, { Component } from "react";
-import FriendBtn from "../../components/FriendBtn";
+// import FriendBtn from "../../components/FriendBtn";
 import API from "../../utils/API";
 import { Link } from "react-router-dom";
 import MainLogo from "../../components/MainLogo";
 
 //----------Friends component----------
 class Friends extends React.Component {
-  
   state = {
-    users:[],
+    users: [],
     uuid: "",
     user: this.props.state
   };
-  
 
   componentDidMount() {
     this.getUsers();
   }
-//Fetching firstName and lastName of users from the db
+  //Fetching firstName and lastName of users from the db
   getUsers = () => {
-    API.getUsers()
-      .then(res =>
-        this.setState({ users: res.data, firstName:"", lastName:""}))
-  }
+    API.getUsers().then(res =>
+      this.setState({ users: res.data, firstName: "", lastName: "" })
+    );
+  };
 
   nextPath(path) {
     // this.state.users(path);
   }
   //-------------Rendering components to the Friends page------------------------
-  render(){
-    let users = this.state.users
+  render() {
+    let users = this.state.users;
     return (
       <div className="container">
-      <div class="Row">
-      <div class="col s12 center-align top:60px">
-        <MainLogo />
+        <div class="Row">
+          <div class="col s12 center-align top:60px">
+            <MainLogo />
+          </div>
+          <div>
+            <h3>WHICH GREEDY BASTARD DO YOU WANT TO BUY FOR?</h3>
+          </div>
+          <div>
+            {users.map(users => (
+              <button type="submit" className="btn btn-info">
+                <Link to={"/FriendRegistry/" + users._id}>
+                  {users.firstName + users.lastName}
+                </Link>
+              </button>
+            ))}
+          </div>
+        </div>
       </div>
-      <div>
-        <h3>WHICH GREEDY BASTARD DO YOU WANT TO BUY FOR?</h3>
-      </div>
-      <div>
-      {users.map(users => <button type="submit" className="btn btn-info"><Link to={"/FriendRegistry/" + users._id}>{users.firstName + users.lastName}</Link></button>)}
-      
-      
-      
-      </div>
-      </div>
-      </div>
-    )
+    );
   }
 }
 
 export default Friends;
-
--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export class Landing extends Component {
   render() {
+    // function friendsRegistry() {
+    //   location.href = "/Friends";
+    // }
+    // function myRegistry() {
+    //   location.href = "/Create";
+    // }
     return (
       <div className="showcase-container">
         <header className="main-header">
           <div className="row">
             <div className="col s12 m12 center">
               <h4>What Do You Bastards Want?</h4>
+              <button>
+                <Link to="/Create" />
+              </button>
             </div>
           </div>
         </header>
